9d8e0e9586991148026d11ce4e8300730de8211c
A.L. Drake <a.l.drake713@gmail.com>
2019-12-02 18:06:38 -0700

    Switch on which part of day one.

--- nineteen/golang/advent1.go
+++ nineteen/golang/advent1.go
@@ -5,6 +5,7 @@ import (
   "io/ioutil"
   "strings"
   "strconv"
+  "os"
 )
 
 func check(e error) {
@@ -18,6 +19,7 @@ func calcFuel(m int64) int64 {
   return (m / 3) - 2
 }
 
+// Calculate the total fuel, as the fuel adds to the total mass.
 func calcTotalFuel(m int64) int64 {
   var total int64
   newFuel := calcFuel(m)
@@ -32,6 +34,10 @@ func main() {
   dat, err := ioutil.ReadFile("../data/1.txt")
   check(err)
 
+  // Determine which fuel algorithm to use.
+  var program int
+  fmt.Sscan(os.Args[1], &program)
+
   var total int64
   for _, v := range strings.Split(string(dat), "\n") {
     if v == "" {
@@ -39,7 +45,14 @@ func main() {
     }
     val, err := strconv.ParseInt(v, 10, 0)
     check(err)
-    total += calcTotalFuel(val)
+    switch program {
+    case 1:
+      total += calcFuel(val)
+    case 2:
+      total += calcTotalFuel(val)
+    default:
+      fmt.Println("Please choose 1 or 2.")
+    }
   }
 fmt.Println(total)
 }
